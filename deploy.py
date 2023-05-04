b84739211bc20ad0fe427d9a4dd3ea45f2ee790a
Aniket Masaye <aniketms42@outlook.com>
2023-05-04 18:40:48 +0000

    new commit

--- deploy.py
+++ deploy.py
@@ -16,6 +16,6 @@ if uploaded_file is not None:
     string_data = stringio.read()
     st.write(string_data)
 
-    # Can be used wherever a "file-like" object is accepted:
+    # Can be used wherever a "file-like" object is accepted: io
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
